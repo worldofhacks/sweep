ca29974a40918aa36c65f2f0ab0cba7e8b426dc8
Rfvooor <saurtree1+rfv@gmail.com>
2026-09-07 15:23:39 +0000

    fix(ohmni): install encoder sampler through vendor plugin loader

---
+++ adapters/ohmni/vendor/sweep_encoder_plugin.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const path = require('path');
+const PairedEncoderSampler = require('./sweep_encoder_plugin/sampler');
+
+function SweepEncoderPlugin(owner) {
+  if (!owner || !owner._serial || !owner._model) {
+    throw new Error('Sweep encoder plugin requires the Telebot serial owner and control model');
+  }
+  if (owner._sweepEncoderPlugin) {
+    throw new Error('Sweep encoder plugin is already installed for this Telebot owner');
+  }
+
+  owner._sweepEncoderPlugin = this;
+  this._sampler = new PairedEncoderSampler(
+    owner._serial,
+    owner._sweepEncoderSocketPath || path.resolve(__dirname, '..', 'sweep_encoder.sock')
+  );
+  this._sampler.start();
+
+  const modelStart = owner._model.start.bind(owner._model);
+  owner._model.start = (...args) => {
+    const result = modelStart(...args);
+    this._sampler.activate();
+    return result;
+  };
+
+  const modelInitialize = owner._model.initialize.bind(owner._model);
+  owner._model.initialize = (...args) => {
+    this._sampler.beginInitialization();
+    return modelInitialize(...args);
+  };
+}
+
+module.exports = SweepEncoderPlugin;
